4927e59fc9c29551ce786b1f2f04815922910071
Richard Weber <git@tebro.simplelogin.com>
2024-12-05 21:46:19 +0200

    Day 5

--- benches/aoc.rs
+++ benches/aoc.rs
@@ -4,6 +4,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 // Add days here
 
 criterion_group!(
@@ -33,7 +34,8 @@ criterion_group!(
     day4::bench_day4_p2,
     day4::bench_day4_p2_alt
 );
+criterion_group!(day5, day5::bench_day5_p1, day5::bench_day5_p2);
 // Add day group here
 
 // Do not remove from last line
-criterion_main!(day1, day2, day3, day4);
+criterion_main!(day1, day2, day3, day4, day5);
